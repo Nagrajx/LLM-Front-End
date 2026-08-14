3f6381e2a9e8332adf19673d88d54160056501f8
MansiSinghRajawat <mansisinghrajawat8@gmail.com>
2026-08-14 11:24:26 +0530

    HighlightText color changed

--- src/components/core/HomePage/HighLightText.jsx
+++ src/components/core/HomePage/HighLightText.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HighlightText = ({text}) => {
   return (
-    <span className="bg-gradient-to-b from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] text-transparent bg-clip-text"> {/* */}
+    <span className="bg-gradient-to-b from-red-500 via-orange-500 to-yellow-500 text-transparent bg-clip-text"> {/* */}
     {text}
   </span>
   )
