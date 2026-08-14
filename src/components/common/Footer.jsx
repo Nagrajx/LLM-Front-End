6a03582c22dabc87bc92d7b801ba2f8e731d68e9
Nagrajx <satishnagraj54@gmail.com>
2026-08-14 12:18:04 +0530

    fix error

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import Logo from "../../assets/Logo/Logo-Full-Light.png"
-import { Link } from 'react-router-dom'
-import { FaFacebook, FaGoogle, FaTwitter, FaYoutube } from "react-icons/fa";
-import { FooterLink2 } from '../../data/footer-links';
+import React from "react";
+import Logo from "../../assets/Logo/Logo-Full-Light.png";
+import { Link } from "react-router-dom";
+import {
+    FaFacebook,
+    FaGoogle,
+    FaTwitter,
+    FaYoutube,
+} from "react-icons/fa";
+import { FooterLink2 } from "../../data/footer-links";
 
 const BottomFooter = ["Privacy Policy", "Cookie Policy", "Terms"];
+
 const Resources = [
     "Articles",
     "Blog",
@@ -15,48 +21,60 @@ const Resources = [
     "Videos",
     "Workspaces",
 ];
-const Plans = ["Paid memberships", "For students", "Business solutions"];
-const Community = ["Forums", "Chapters", "Events"];
+
+const Plans = [
+    "Paid memberships",
+    "For students",
+    "Business solutions",
+];
+
+const Community = [
+    "Forums",
+    "Chapters",
+    "Events",
+];
+
 const Footer = () => {
     return (
-        <div className='bg-[#161D29]'>
-            <div className='w-11/12 max-w-[1260px] mx-auto flex flex-col text-[#6E727F] relative py-14'>
+        <div className="bg-[#161D29]">
+            <div className="w-11/12 max-w-[1260px] mx-auto flex flex-col text-[#6E727F] relative py-14">
 
-                {/* upper footer  */}
-                <div className='w-full border-b flex flex-col lg:flex-row flex-wrap pb-5 border-[#2C333F]'>
+                {/* Upper Footer */}
+                <div className="w-full border-b flex flex-col lg:flex-row flex-wrap pb-5 border-[#2C333F]">
 
-                    {/* section1  */}
-                    <div className='lg:w-[50%] flex flex-wrap lg:gap-0 gap-5 justify-between lg:border-r border-[#2C333F]'>
+                    {/* Section 1 */}
+                    <div className="lg:w-[50%] flex flex-wrap lg:gap-0 gap-5 justify-between lg:border-r border-[#2C333F]">
 
-                        <div className='lg:w-[30%] flex flex-col gap-3'>
-                            <img src={Logo}
+                        {/* Company */}
+                        <div className="lg:w-[30%] flex flex-col gap-3">
+                            <img
+                                src={Logo}
                                 alt=""
-                                className='object-contain'
+                                className="object-contain"
                             />
 
-                            <h2 className='text-[#DBDDEA] font-semibold text-[16px]'>
+                            <h2 className="text-[#DBDDEA] font-semibold text-[16px]">
                                 Codemitra Technology....
                             </h2>
 
-                            <div className='flex flex-col gap-2'>
-                                {
-                                    ["About", "Careers", "Affiliates"].map((element, index) => {
+                            <div className="flex flex-col gap-2">
+                                {["About", "Careers", "Affiliates"].map(
+                                    (element, index) => {
                                         return (
                                             <div
                                                 key={index}
-                                                className='text-[14px] cursor-pointer hover:text-[#C5C7D4] transition-all duration-200'>
-
+                                                className="text-[14px] cursor-pointer hover:text-[#C5C7D4] transition-all duration-200"
+                                            >
                                                 <Link to={element.toLowerCase()}>
                                                     {element}
                                                 </Link>
-
                                             </div>
-                                        )
-                                    })
-                                }
+                                        );
+                                    }
+                                )}
                             </div>
 
-                            <div className='flex gap-3 text-lg'>
+                            <div className="flex gap-3 text-lg">
                                 <FaFacebook />
                                 <FaGoogle />
                                 <FaTwitter />
@@ -64,153 +82,161 @@ const Footer = () => {
                             </div>
                         </div>
 
-
-                        <div className='lg:w-[30%] w-[48%] '>
-                            {/* Resources */}
+                        {/* Resources */}
+                        <div className="lg:w-[30%] w-[48%]">
                             <div>
-                                <p className='text-[#C5C7D4] font-semibold text-[16px]'>
+                                <p className="text-[#C5C7D4] font-semibold text-[16px]">
                                     Resources
                                 </p>
 
-                                <div className='flex flex-col gap-2 mt-2'>
-                                    {
-                                        Resources.map((element, index) => {
-                                            return (
-                                                <div
-                                                    key={index}
-                                                    className='text-[14px] cursor-pointer hover:text-[#C5C7D4] transition-all duration-200'>
-
-                                                    <Link to={element.toLowerCase()}>
-                                                        {element}
-                                                    </Link>
-                                                </div>
-                                            )
-                                        })
-                                    }
+                                <div className="flex flex-col gap-2 mt-2">
+                                    {Resources.map((element, index) => {
+                                        return (
+                                            <div
+                                                key={index}
+                                                className="text-[14px] cursor-pointer hover:text-[#C5C7D4] transition-all duration-200"
+                                            >
+                                                <Link to={element.toLowerCase()}>
+                                                    {element}
+                                                </Link>
+                                            </div>
+                                        );
+                                    })}
                                 </div>
                             </div>
 
                             {/* Support */}
-                            <div className='mt-7'>
+                            <div className="mt-7">
+                                <p className="text-[#C5C7D4] font-semibold text-[16px]">
+                                    Support
+                                </p>
 
-                                <p className='text-[#C5C7D4] font-semibold text-[16px]'>Support</p>
-
-                                <div className='text-[14px] cursor-pointer hover:text-[#C5C7D4] transition-all duration-200 mt-2'>
-                                    <Link to={"/help-center"}>Help Center</Link>
+                                <div className="text-[14px] cursor-pointer hover:text-[#C5C7D4] transition-all duration-200 mt-2">
+                                    <Link to="/help-center">
+                                        Help Center
+                                    </Link>
                                 </div>
                             </div>
-
                         </div>
 
-
-                        <div className='lg:w-[30%] w-[48%] '>
-                            {/* plans  */}
+                        {/* Plans & Community */}
+                        <div className="lg:w-[30%] w-[48%]">
+                            {/* Plans */}
                             <div>
-                                <p className='text-[#C5C7D4] font-semibold text-[16px]'>Plans</p>
+                                <p className="text-[#C5C7D4] font-semibold text-[16px]">
+                                    Plans
+                                </p>
 
-                                <div className='flex flex-col gap-2 mt-2'>
-                                    {
-                                        Plans.map((element, index) => {
-                                            return (
-                                                <div
-                                                    key={index}
-                                                    className='text-[14px] cursor-pointer hover:tex-[#C5C7D4] transition-all duration-200'>
-                                                    <Link to={element.split(" ").join("-").toLowerCase()}>{element}</Link>
-                                                </div>
-                                            )
-                                        })
-                                    }
+                                <div className="flex flex-col gap-2 mt-2">
+                                    {Plans.map((element, index) => {
+                                        return (
+                                            <div
+                                                key={index}
+                                                className="text-[14px] cursor-pointer hover:text-[#C5C7D4] transition-all duration-200"
+                                            >
+                                                <Link
+                                                    to={element
+                                                        .split(" ")
+                                                        .join("-")
+                                                        .toLowerCase()}
+                                                >
+                                                    {element}
+                                                </Link>
+                                            </div>
+                                        );
+                                    })}
                                 </div>
                             </div>
 
                             {/* Community */}
-                            <div className='mt-7'>
+                            <div className="mt-7">
+                                <p className="text-[#C5C7D4] font-semibold text-[16px]">
+                                    Community
+                                </p>
 
-                                <p className='text-[#C5C7D4] font-semibold text-[16px]'>Community</p>
-
-                                <div className='flex flex-col gap-2 mt-2'>
-                                    {
-                                        Community.map((element, index) => {
-                                            return (
-                                                <div
-                                                    key={index}
-                                                    className='text-[14px] cursor-pointer hover:text-[#C5C7D4] transition-all duration-200'>
-                                                    <Link to={element.toLowerCase()}>{element}</Link>
-                                                </div>
-                                            )
-                                        })
-                                    }
+                                <div className="flex flex-col gap-2 mt-2">
+                                    {Community.map((element, index) => {
+                                        return (
+                                            <div
+                                                key={index}
+                                                className="text-[14px] cursor-pointer hover:text-[#C5C7D4] transition-all duration-200"
+                                            >
+                                                <Link to={element.toLowerCase()}>
+                                                    {element}
+                                                </Link>
+                                            </div>
+                                        );
+                                    })}
                                 </div>
                             </div>
                         </div>
-
                     </div>
 
-                    {/* section 2  */}
-                    <div className='lg:w-[50%] flex flex-wrap gap-5 lg:gap-0 justify-between lg:mt-0 mt-5 lg:pl-5 '>
-                        {
-                            FooterLink2.map((heading, index) => {
-                                return (
-                                    <div
-                                        key={index}
-                                        className='lg:w-[30%] w-[48%] '>
-                                        <h1 className="text-[#C5C7D4] font-semibold text-[16px]">
-                                            {heading.title}
-                                        </h1>
+                    {/* Section 2 */}
+                    <div className="lg:w-[50%] flex flex-wrap gap-5 lg:gap-0 justify-between lg:mt-0 mt-5 lg:pl-5">
+                        {FooterLink2.map((heading, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    className="lg:w-[30%] w-[48%]"
+                                >
+                                    <h1 className="text-[#C5C7D4] font-semibold text-[16px]">
+                                        {heading.title}
+                                    </h1>
 
-                                        <div className='flex flex-col gap-2 mt-2'>
-                                            {
-                                                heading.links.map((link, i) => {
-                                                    return (
-                                                        <div
-                                                            key={i}
-                                                            className='text-[14px] cursor-pointer hover:text-[#C5C7D4] transition-all duration-200'>
-                                                            <Link to={link.link}>
-                                                                {link.title}
-                                                            </Link>
-                                                        </div>
-                                                    )
-                                                })
-                                            }
-                                        </div>
-
+                                    <div className="flex flex-col gap-2 mt-2">
+                                        {heading.links.map((link, i) => {
+                                            return (
+                                                <div
+                                                    key={i}
+                                                    className="text-[14px] cursor-pointer hover:text-[#C5C7D4] transition-all duration-200"
+                                                >
+                                                    <Link to={link.link}>
+                                                        {link.title}
+                                                    </Link>
+                                                </div>
+                                            );
+                                        })}
                                     </div>
-                                )
-                            })
-                        }
+                                </div>
+                            );
+                        })}
                     </div>
-
-
-
                 </div>
 
-                {/* bottom footer  */}
-                <div className='flex justify-between text-[#6E727F] mt-14 lg:flex-row flex-col items-center gap-y-3'>
-                    <div className='flex gap-3 lg:items-start items-center flex-row'>
-                        {
-                            BottomFooter.map((element, index) => {
-                                return (
-                                    <div
-                                        key={index}
-                                        className={`text-[14px] cursor-pointer hover:text-[#C5C7D4] transition-all duration-200 px-3
-                                            ${index === BottomFooter.length - 1 ? "" : "border-r border-[#2C333F]"}`}>
-                                        <Link to={element.split(" ").join("-").toLowerCase()}>
-                                            {element}
-                                        </Link>
-                                    </div>
-                                )
-                            })
-                        }
+                {/* Bottom Footer */}
+                <div className="flex justify-between text-[#6E727F] mt-14 lg:flex-row flex-col items-center gap-y-3">
+                    <div className="flex gap-3 lg:items-start items-center flex-row">
+                        {BottomFooter.map((element, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    className={`text-[14px] cursor-pointer hover:text-[#C5C7D4] transition-all duration-200 px-3 ${
+                                        index === BottomFooter.length - 1
+                                            ? ""
+                                            : "border-r border-[#2C333F]"
+                                    }`}
+                                >
+                                    <Link
+                                        to={element
+                                            .split(" ")
+                                            .join("-")
+                                            .toLowerCase()}
+                                    >
+                                        {element}
+                                    </Link>
+                                </div>
+                            );
+                        })}
                     </div>
 
-                    <div className='text-[14px]'>
+                    <div className="text-[14px]">
                         Made with ❤️ By Satish Nagraj © 2026 StudyNotion
                     </div>
                 </div>
-
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
